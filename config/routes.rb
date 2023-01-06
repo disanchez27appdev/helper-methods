62190adc3ac2b1759026f455ee94fcf0f356923b
disanchez27edu <disanchez@uchicago.edu>
2023-01-06 22:14:41 +0000

    Declared prefixes for route paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  get "/" => "movies#index"
+  root "movies#index"
 
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create"
-  get "/movies/new" => "movies#new"
+  post "/movies" => "movies#create" #as movies_path and movies_url
+  get "/movies/new" => "movies#new" as :new_movie # as :new_movie_path and new_movie_url
           
   # READ
-  get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show"
+  get "/movies" => "movies#index" 
+  get "/movies/:id" => "movies#show" as :movie # movie_path() needs an argument to populate :id
   
   # UPDATE
   patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit"
+  get "/movies/:id/edit" => "movies#edit" as :movie_edit # movie_edit_path()
   
   # DELETE
   delete "/movies/:id" => "movies#destroy"
