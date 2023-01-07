a3cdbc1d030566c79077e77669bdfd468ef50f21
disanchez27edu <disanchez@uchicago.edu>
2023-01-07 01:09:23 +0000

    Refactor complete

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
 
   # CREATE
   post "/movies" => "movies#create" #as movies_path and movies_url
-  get "/movies/new" => "movies#new" as :new_movie # as :new_movie_path and new_movie_url
+  get "/movies/new" => "movies#new", as: :new_movie # as :new_movie_path and new_movie_url
           
   # READ
-  get "/movies" => "movies#index" 
-  get "/movies/:id" => "movies#show" as :movie # movie_path() needs an argument to populate :id
+  get "/movies" => "movies#index"
+  get "/movies/:id" => "movies#show", as: :movie # movie_path() needs an argument to populate :id
   
   # UPDATE
   patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit" as :movie_edit # movie_edit_path()
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie # movie_edit_path()
   
   # DELETE
   delete "/movies/:id" => "movies#destroy"
